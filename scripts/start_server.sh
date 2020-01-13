a53c069168fe7e2df49b739e73e99de46f92d8e5
Suman Khara <suman.khara@cerner.com>
2020-01-13 15:53:41 +0530

    modified scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /home/ec2-user/spring-boot-demo-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar /home/ec2-user/aws-spring-boot-demo-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
